44dad829299900d6c55b09f9ea8be982aa40f719
Benjamin Gustin <benjamin.gustin@iguanesolutions.com>
2019-12-18 13:49:29 +0100

    add event types

--- webhook_v2.go
+++ webhook_v2.go
@@ -5,6 +5,25 @@ import (
 	"io"
 )
 
+const (
+	// IncidentTrigger Webhook Event Type
+	IncidentTrigger string = "incident.trigger"
+	// IncidentAcknowledge Webhook Event Type
+	IncidentAcknowledge string = "incident.acknowledge"
+	// IncidentUnacknowledge Webhook Event Type
+	IncidentUnacknowledge string = "incident.unacknowledge"
+	// IncidentResolve Webhook Event Type
+	IncidentResolve string = "incident.resolve"
+	// IncidentAssign Webhook Event Type
+	IncidentAssign string = "incident.assign"
+	// IncidentEscalate Webhook Event Type
+	IncidentEscalate string = "incident.escalate"
+	// IncidentDelegate Webhook Event Type
+	IncidentDelegate string = "incident.delegate"
+	// IncidentAnnotate Webhook Event Type
+	IncidentAnnotate string = "incident.annotate"
+)
+
 // V2WebhookPayload is a list of messages for a webhook.
 type V2WebhookPayload struct {
 	Messages []Message `json:"messages"`
@@ -14,7 +33,7 @@ type V2WebhookPayload struct {
 type Message struct {
 	ID         string     `json:"id"`
 	Event      string     `json:"event"`
-	CreatedOn  string     `json:"created_on"` // TODO: time.Time
+	CreatedOn  string     `json:"created_on"`
 	LogEntries []LogEntry `json:"log_entries"`
 	Webhook    Webhook    `json:"webhook"`
 	Incident   Incident   `json:"incident"`
